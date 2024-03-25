d50c7876e07b5dedbd174567e2142547e4b40435
Christian Bourgeois <christian.bourgeois@gargoyles.online>
2024-03-24 20:24:46 -0400

    Plugins can be added to tokenizer.

--- src/tokenizer/index.ts
+++ src/tokenizer/index.ts
@@ -1,31 +1,41 @@
-import MarkdownIt from 'markdown-it/lib';
-import annotations from './plugins/annotations';
-import frontmatter from './plugins/frontmatter';
-import comments from './plugins/comments';
-import type Token from 'markdown-it/lib/token';
+import MarkdownIt from "markdown-it/lib";
+import annotations from "./plugins/annotations";
+import frontmatter from "./plugins/frontmatter";
+import comments from "./plugins/comments";
+import type Token from "markdown-it/lib/token";
 
 export default class Tokenizer {
-  private parser: MarkdownIt;
+	private parser: MarkdownIt;
 
-  constructor(
-    config: MarkdownIt.Options & {
-      allowIndentation?: boolean;
-      allowComments?: boolean;
-    } = {}
-  ) {
-    this.parser = new MarkdownIt(config);
-    this.parser.use(annotations, 'annotations', {});
-    this.parser.use(frontmatter, 'frontmatter', {});
-    this.parser.disable([
-      'lheading',
-      // Disable indented `code_block` support https://spec.commonmark.org/0.30/#indented-code-block
-      'code',
-    ]);
+	constructor(config: TokenizerOptions = {}) {
+		this.parser = new MarkdownIt(config);
+		this.parser.use(annotations, "annotations", {});
+		this.parser.use(frontmatter, "frontmatter", {});
+		this.parser.disable([
+			"lheading",
+			// Disable indented `code_block` support https://spec.commonmark.org/0.30/#indented-code-block
+			"code",
+		]);
 
-    if (config.allowComments) this.parser.use(comments, 'comments', {});
-  }
+		if (config.allowComments) this.parser.use(comments, "comments", {});
+	}
 
-  tokenize(content: string): Token[] {
-    return this.parser.parse(content.toString(), {});
-  }
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	use(plugin: MarkdownItPlugin, args: any) {
+		this.parser = this.parser.use(plugin, args);
+	}
+
+	tokenize(content: string): Token[] {
+		return this.parser.parse(content.toString(), {});
+	}
 }
+
+type MarkdownItPlugin =
+	| MarkdownIt.PluginSimple
+	| MarkdownIt.PluginWithOptions
+	| MarkdownIt.PluginWithParams;
+
+type TokenizerOptions = MarkdownIt.Options & {
+	allowIndentation?: boolean;
+	allowComments?: boolean;
+};
